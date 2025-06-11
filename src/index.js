d9c3c58164a9d5fe7615e61034bb71a57e4ac0c4
aj2nd <anandhujayaprakash@gmail.com>
2025-06-11 22:16:15 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
   res.send('Hello, Jarvis!');
+});
+
+// Route to serve dashboard.html
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 });
 
 app.listen(port, () => {
